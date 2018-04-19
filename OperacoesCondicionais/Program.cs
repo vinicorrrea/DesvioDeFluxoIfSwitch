a8a567b9acdd401c94d1e7789f8dd4d378c93b11
Vinicius Corrêa <vinicius-correa2007@bol.com.br>
2018-04-19 11:11:31 -0300

    Declaração de Variáveis

--- OperacoesCondicionais/Program.cs
+++ OperacoesCondicionais/Program.cs
@@ -6,7 +6,21 @@ namespace OperacoesCondicionais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Comentario em linha (in-line)
+            /*
+                Comentario em bloco (in-block)
+            */
+
+            // Declaração da variavel peração, ela sera do tipo inteira -numeros inteiros- (int)
+            int operacao;
+
+            // Declaração das variaveis n1 e n2, elas seram do tipo double -numeros quebrados- (double)
+            double n1, n2;
+
+            
+
+
+
         }
     }
 }
