40662dd142cc60d42445078e01d7a4940b0db9e0
Vinicius Corrêa <vinicius-correa2007@bol.com.br>
2018-04-19 12:10:16 -0300

    Código Completo

--- OperacoesCondicionais/Program.cs
+++ OperacoesCondicionais/Program.cs
@@ -34,7 +34,29 @@ namespace OperacoesCondicionais
 
             Console.WriteLine("Digite outro número");
             n2 = double.Parse(Console.ReadLine());
-            
+
+            if(operacao==1)
+            {
+                Console.WriteLine("O resultado da soma é: "+(n1+n2));
+            }
+            else if(operacao==2)
+            {
+                Console.WriteLine("O resultado da subtração é: "+(n1-n2));
+            }
+            else if(operacao==3)
+            {
+                Console.WriteLine("O resultado da multiplicação é: "+(n1*n2));
+            }
+            else if(operacao==4)
+            {
+                Console.WriteLine("O resultado da divisão é: "+(n1/n2));
+            }
+            else
+            {
+                Console.WriteLine("Operação Inválida!");
+            }
+
+
 
 
 
